e7efb964f251ac9d416d866da8d4b9f6b2e3ef53
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-08 16:22:53 +0800

    Update test_acosh.py
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- benchmark/test_acosh.py
+++ benchmark/test_acosh.py
@@ -12,3 +12,13 @@ def test_acosh():
         dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
+
+@pytest.mark.acosh_
+def test_acosh_():
+    bench = base.UnaryPointwiseBenchmark(
+        op_name="acosh_",
+        torch_op=torch.acosh_,         
+        dtypes=consts.FLOAT_DTYPES,
+        is_inplace=True,
+    )
+    bench.run()
