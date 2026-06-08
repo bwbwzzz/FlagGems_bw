7d361cc9377e4b007bafb1049816aecab20730b7
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-08 16:43:10 +0800

    Implement test for acosh with parameterized inputs
    Add parameterized test for torch.acosh function.
    
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- tests/test_acosh.py
+++ tests/test_acosh.py
@@ -19,3 +19,17 @@ def test_acosh(shape, dtype):
         res_out = torch.acosh(inp)
 
     utils.gems_assert_close(res_out, ref_out, dtype)
+
+
+@pytest.mark.acosh_
+@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_acosh_(shape, dtype):
+    inp = torch.empty(shape, dtype=dtype, device=flag_gems.device).uniform_(1.0, 2.0)
+    ref_inp = utils.to_reference(inp.clone())
+
+    ref_out = torch.acosh_(ref_inp)
+    with flag_gems.use_gems():
+        res_out = torch.acosh_(inp)
+
+    utils.gems_assert_close(res_out, ref_out, dtype)
