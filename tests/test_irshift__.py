da8b20e52a0d2d484f500bba6db726a93e120928
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-25 16:23:59 +0800

    Update test_irshift__.py
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- tests/test_irshift__.py
+++ tests/test_irshift__.py
@@ -5,12 +5,6 @@ import flag_gems
 
 from . import accuracy_utils as utils
 
-# Shapes that work with __irshift__ (no broadcasting required)
-IRSHIFT_SHAPES = [
-    ((512, 1024), (512, 1024)),
-    ((1024,), ()),
-]
-
 # Shapes that support inplace bitwise operations with broadcasting
 INPLACE_BITWISE_SHAPES = [
     ((512, 1024), (512, 1024)),
@@ -21,19 +15,27 @@ INPLACE_BITWISE_SHAPES = [
 
 
 @pytest.mark.irshift__
-@pytest.mark.parametrize("shapes", IRSHIFT_SHAPES)
+@pytest.mark.parametrize("shapes", INPLACE_BITWISE_SHAPES)
 @pytest.mark.parametrize("dtype", utils.ALL_INT_DTYPES + [torch.uint8])
 def test_irshift__(shapes, dtype):
     shape_a, shape_b = shapes
+
     res_a = torch.randint(0, 100, shape_a, dtype=dtype, device="cpu").to(
         flag_gems.device
     )
     res_b = torch.randint(0, 8, shape_b, dtype=dtype, device="cpu").to(flag_gems.device)
-    ref_a = utils.to_reference(res_a)
+
+    ref_a = utils.to_reference(res_a.clone())
     ref_b = utils.to_reference(res_b)
 
-    # Use bitwise_right_shift as reference since __irshift__ has issues with broadcasting
-    ref_out = torch.bitwise_right_shift(ref_a, ref_b)
+    # PyTorch reference: modify ref_a in-place.
+    ref_a.__irshift__(ref_b)
+
+    data_ptr = res_a.data_ptr()
+
     with flag_gems.use_gems():
-        res_out = torch.ops.aten.__irshift__(res_a, res_b)
-    utils.gems_assert_close(res_out, ref_out, dtype)
+        ret = res_a.__irshift__(res_b)
+
+    assert ret is res_a
+    assert res_a.data_ptr() == data_ptr
+    utils.gems_assert_close(res_a, ref_a, dtype)
