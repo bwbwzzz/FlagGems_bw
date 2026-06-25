cdbc9089fd63ee56d07e1dba222ed70b92b9286b
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-25 18:38:30 +0800

    Remove unused import act_quant_triton
    Removed unused import for act_quant_triton.
    
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -29,7 +29,6 @@ from flag_gems.ops.abs import abs, abs_
 from flag_gems.ops.absolute import absolute
 from flag_gems.ops.acos import acos
 from flag_gems.ops.acosh import acosh, acosh_
-from flag_gems.ops.act_quant import act_quant_triton
 from flag_gems.ops.adaptive_avg_pool2d import adaptive_avg_pool2d
 from flag_gems.ops.adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from flag_gems.ops.add import add, add_
