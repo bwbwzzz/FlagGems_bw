0f911d7351ee0fddae293604f17699ed7adae70e
bwbwzzz <957058626@qq.com>
2026-06-26 07:32:30 +0000

    [KernelGen][Nvidia] Add reduce_max operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -516,6 +516,7 @@ _FULL_CONFIG = (
     ("randperm", randperm),
     ("reciprocal", reciprocal),
     ("reciprocal_", reciprocal_),
+    ("reduce_max", reduce_max),
     ("reflection_pad1d", reflection_pad1d),
     ("reflection_pad1d.out", reflection_pad1d_out),
     ("reflection_pad1d_backward", reflection_pad1d_backward),
