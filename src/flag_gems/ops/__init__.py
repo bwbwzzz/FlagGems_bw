9be32f7c4c24816270287eb6ee80ccb9dc57cf7b
bwbwzzz <bwbwzzz@users.noreply.github.com>
2026-07-01 13:44:21 +0800

    Fix isort ordering in ops/__init__.py
    Move special_scaled_modified_bessel_k0 import after special_log_softmax
    to satisfy isort (alphabetical by module name), fixing the code-style CI.

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -447,10 +447,10 @@ from flag_gems.ops.special_gammainc import special_gammainc
 from flag_gems.ops.special_hermite_polynomial_h import special_hermite_polynomial_h
 from flag_gems.ops.special_i0e import special_i0e, special_i0e_out
 from flag_gems.ops.special_i1 import special_i1, special_i1_out
+from flag_gems.ops.special_log_softmax import special_log_softmax
 from flag_gems.ops.special_scaled_modified_bessel_k0 import (  # noqa: F401
     scaled_modified_bessel_k0,
 )
-from flag_gems.ops.special_log_softmax import special_log_softmax
 from flag_gems.ops.special_shifted_chebyshev_polynomial_u import (
     special_shifted_chebyshev_polynomial_u,
     special_shifted_chebyshev_polynomial_u_,
@@ -1057,8 +1057,8 @@ __all__ = [
     "special_i0e_out",
     "special_i1",
     "special_i1_out",
-    "special_scaled_modified_bessel_k0",
     "special_log_softmax",
+    "special_scaled_modified_bessel_k0",
     "special_shifted_chebyshev_polynomial_u",
     "special_shifted_chebyshev_polynomial_u_",
     "split_with_sizes_copy",
