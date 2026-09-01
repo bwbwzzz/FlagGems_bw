da55fabc268727980eeabfea6904c922795edc26
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-09-01 11:59:58 +0800

    test: call flag_gems ops directly instead of use_gems

--- tests/test_t_.py
+++ tests/test_t_.py
@@ -29,8 +29,7 @@ def test_t_(shape, dtype):
     x = torch.randn(shape, dtype=dtype, device=flag_gems.device)
     ref_x = utils.to_reference(x)
     ref_out = torch.ops.aten.t_(ref_x)
-    with flag_gems.use_gems():
-        act_out = torch.ops.aten.t_(x)
+    act_out = flag_gems.t_(x)
     utils.gems_assert_close(act_out, ref_out, dtype)
 
 
@@ -42,6 +41,5 @@ def test_t_1d(shape, dtype):
     x = torch.randn(shape, dtype=dtype, device=flag_gems.device)
     ref_x = utils.to_reference(x)
     ref_out = torch.ops.aten.t_(ref_x)
-    with flag_gems.use_gems():
-        act_out = torch.ops.aten.t_(x)
+    act_out = flag_gems.t_(x)
     utils.gems_assert_close(act_out, ref_out, dtype)
