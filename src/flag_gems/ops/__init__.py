e08851dd197dac0e7993a6bc4c051bfe7394048c
bwbwzzz <957058626@qq.com>
2026-09-20 09:27:20 +0000

    [KernelGen][Nvidia] Add softshrink_backward operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -976,6 +976,7 @@ from flag_gems.ops.softmax import (
 )
 from flag_gems.ops.softplus import softplus, softplus_backward
 from flag_gems.ops.softshrink import softshrink, softshrink_out
+from flag_gems.ops.softshrink_backward import softshrink_backward
 from flag_gems.ops.sort import sort, sort_stable
 from flag_gems.ops.sparse_sampled_addmm import (
     sparse_sampled_addmm,
@@ -2218,6 +2219,7 @@ __all__ = [
     "softplus",
     "softplus_backward",
     "softshrink",
+    "softshrink_backward",
     "softshrink_out",
     "sort",
     "sort_stable",
